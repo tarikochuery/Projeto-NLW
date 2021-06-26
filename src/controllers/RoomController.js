018c1d230297a80d505de592ecd1d652844afe61
tarikochuery <ttarikechuery@gmail.com>
2021-06-25 23:39:39 -0300

    Projeto Finalizado

--- src/controllers/RoomController.js
+++ src/controllers/RoomController.js
@@ -5,21 +5,58 @@ module.exports = {
         const db = await Database()
         const pass = req.body.password
         let roomId
+        let isRoom = true
 
-        for(var i = 0; i < 6; i++){
-            i == 0 ? roomId = Math.floor(Math.random() * 10).toString() :
-            roomId += Math.floor(Math.random() * 10).toString()
+        while(isRoom){
+
+            // Gera o número da sala
+            for(var i = 0; i < 6; i++){
+                i == 0 ? roomId = Math.floor(Math.random() * 10).toString() :
+                roomId += Math.floor(Math.random() * 10).toString()
+            }
+    
+            // Verificar se o número já existe
+    
+            const roomsExistIds = await db.all(`SELECT id FROM rooms`)
+    
+            isRoom = roomsExistIds.some(roomExistId => roomExistId === roomId)
+    
+            if(!isRoom){
+                // Insere a sala no banco
+                await db.run(`INSERT INTO rooms (
+                    id,
+                    pass
+                ) VAlUES (
+                    ${parseInt(roomId)},
+                    ${pass}
+                )`)    
+            }
+
         }
 
-        await db.run(`INSERT INTO rooms (
-            id,
-            pass
-        ) VAlUES (
-            ${parseInt(roomId)},
-            ${pass}
-        )`)
 
         await db.close()
+
+        res.redirect(`/room/${roomId}`)
+    },
+
+    async open(req, res){
+        const db = await Database()
+        const roomId = req.params.room
+        const questions = await db.all(`SELECT * FROM questions WHERE room = ${roomId} AND read = 0`)
+        const questionsRead = await db.all(`SELECT * FROM questions WHERE room = ${roomId} AND read = 1`)
+        let isNoQuestion
+        if(questions.length == 0 ){
+            if(questionsRead.length==0){
+                isNoQuestion = true
+            }
+        }
+
+        res.render('room', {roomId: roomId, questions: questions, questionsRead: questionsRead, isNoQuestion: isNoQuestion})
+    },
+
+    enter(req, res){
+        const roomId = req.body.roomId
 
         res.redirect(`/room/${roomId}`)
     }
--- src/db/init.js
+++ src/db/init.js
@@ -1,22 +1,23 @@
-const Database = require("./config")
+const Database = require('./config')
 
 const initDb = {
-    async init(){
+    async init() {
         const db = await Database()
 
-        await db.exec(`CREATE TABLE rooms (
+        await db.exec(`CREATE TABLE rooms(
             id INTEGER PRIMARY KEY,
             pass TEXT
-        )`);
+        )`)
 
-        await db.exec(`CREATE TABLE questions (
+        await db.exec(`CREATE TABLE questions(
             id INTEGER PRIMARY KEY AUTOINCREMENT,
-            titulo TEXT,
-            read INT 
-        )`);
+            title TEXT,
+            read INT,
+            room, INT
+        )`)
 
         await db.close()
     }
 }
 
-initDb.init();
+initDb.init()
